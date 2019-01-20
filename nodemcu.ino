4ab5c95ad186b982c109fd45497ccd203b1ac25f
Ibrahim Ullah <34267927+Ibrahim-ullah@users.noreply.github.com>
2019-01-20 22:18:03 +0600

    Add files via upload
    Nodemcu sends a long string which consist of all the time duration adding the plus sign.

---
+++ nodemcu.ino
@@ -0,0 +1,44 @@
+
+
+#include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+
+// Set these to run example.
+#define FIREBASE_HOST "homecontrol-7622c.firebaseio.com"
+#define FIREBASE_AUTH "guGbFq5GTTbA1EDTlVGfrqEUojgFxLUMs5K78Hja"
+#define WIFI_SSID "Redmi"
+#define WIFI_PASSWORD "0987654321"
+String value = "";
+String first,second,third,fourth;
+String var;
+
+void setup() 
+{
+Serial.begin(9600);
+Wire.begin(D1,D2);
+  // connect to wifi.
+WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+Serial.print("connecting");
+while (WiFi.status() != WL_CONNECTED) 
+{
+Serial.print(".");
+delay(500);
+}
+
+Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+int n = 0;
+void loop()
+{
+Wire.beginTransmission(8); 
+first = Firebase.getString("first");      //getting string from first node
+second = Firebase.getString("second");
+third = Firebase.getString("third");
+fourth = Firebase.getString("fourth");
+var = first +"+"+ second+ "+" +third+ "+" +fourth;
+Serial.println(first);
+ Wire.write(var.c_str());  //convert the string into character buffer
+ Wire.endTransmission();    
+
+}
---
+++ uno.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+String data = "";   //String for collecting data from wire.read
+int index = 20;       //defining a maximum index that is possible
+void setup() {
+ Wire.begin(8);                /* join i2c bus with address 8 */
+ Wire.onReceive(receiveEvent); /* register receive event */
+ Serial.begin(9600);           /* start serial for debug */
+}
+
+void loop() {
+ delay(100);
+ for (int i = 0; i < 20; i++)
+ {
+  Serial.print(data[i]);
+ }
+}
+
+// function that executes whenever data is received from master
+void receiveEvent(int howMany) {
+ while (0 <Wire.available())
+ {
+  if ( index  > 1)
+  {
+    char c = Wire.read();      /* receive byte as a character */
+    data += c;
+    index--;
+  }          /* print the character */
+  }
+ Serial.println();             /* to newline */
+}
+
+// function that executes whenever data is requested from master
+
