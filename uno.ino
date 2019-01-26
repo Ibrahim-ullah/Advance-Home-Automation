6a037409160894b2db50fafc3a6bb75192b1e7a8
Ibrahim Ullah <34267927+Ibrahim-ullah@users.noreply.github.com>
2019-01-26 14:10:36 +0600

    Update uno.ino
    turning of load one by one according to the user given time.the loads are  on when the power is given.

--- uno.ino
+++ uno.ino
@@ -1,5 +1,5 @@
 
-String data = "1+2+3+4+.1+2+3+4.1+2+3+4.1+2+3+4.";
+String data = "30+20+10+5+.1+2+3+4.1+2+3+4.1+2+3+4.";
 int val[] = {0, 0, 0, 0};
 char variable[20];
 char c = '+';
@@ -10,10 +10,14 @@ unsigned long previousMillis1 = 0;
 unsigned long previousMillis2 = 0;
 unsigned long previousMillis3 = 0;
 unsigned long previousMillis4 = 0;
-bool ledState1 = false;
-bool ledState2 = false;
-bool ledState3 = false;
-bool ledState4 = false;
+bool ledState1 = true;
+bool ledState2 = true;
+bool ledState3 = true;
+bool ledState4 = true;
+int flag1 = 0;
+int flag2 = 0;
+int flag3 = 0;
+int flag4 = 0;
 
 void setup()
 {
@@ -27,7 +31,7 @@ void setup()
  digitalWrite(12, ledState3);
   pinMode(13, OUTPUT);
  digitalWrite(13, ledState4);
- collecting_duratoin();
+ collecting_data();
 }
 
 void loop()
@@ -36,41 +40,45 @@ void loop()
    unsigned long currentMillis = millis(); // grab current time
  
  
-
- if ((unsigned long)(currentMillis - previousMillis1) >= interval1)
+//  check if "interval" time has passed (1000 milliseconds)
+ if (((unsigned long)(currentMillis - previousMillis1) >= interval1) && flag1 == 0)
  {
    ledState1 = !ledState1; // "toggles" the state
    digitalWrite(7, ledState1); // sets the LED based on ledState
    // save the "current" time
    previousMillis1 = millis();
+   flag1++;
  }
  
-   if ((unsigned long)(currentMillis - previousMillis2) >= interval2)
+   if (((unsigned long)(currentMillis - previousMillis2) >= interval2) && flag2 == 0)
  {
    ledState2 = !ledState2; // "toggles" the state
    digitalWrite(8, ledState2); // sets the LED based on ledState
    // save the "current" time
    previousMillis2 = millis();
+   flag2++;
  }
-  if ((unsigned long)(currentMillis - previousMillis3) >= interval3)
+  if (((unsigned long)(currentMillis - previousMillis3) >= interval3) && flag3 == 0)
  {
    ledState3 = !ledState3; // "toggles" the state
    digitalWrite(12, ledState3); // sets the LED based on ledState
    // save the "current" time
    previousMillis3 = millis();
+   flag3++;
  }
-  if ((unsigned long)(currentMillis - previousMillis4) >= interval4)
+  if (((unsigned long)(currentMillis - previousMillis4) >= interval4) && flag4 == 0)
  {
    ledState4 = !ledState4; // "toggles" the state
    digitalWrite(13, ledState4); // sets the LED based on ledState
    // save the "current" time
    previousMillis4 = millis();
+   flag4++;
  }
 // while(1)
 //  {}
 }
 
-void collecting_duration()
+void collecting_data()
 {
    for (int i = 0; i < 20 ; i++)
   {
@@ -93,8 +101,6 @@ void collecting_duration()
    interval3 = (load3.toInt())*1000;
    interval4 = (load4.toInt())*1000;
 }
-
-
 
 
 
